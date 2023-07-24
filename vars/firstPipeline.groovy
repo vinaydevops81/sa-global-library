a532d83e3590ef8d4e95ba0d300bf02777534959
vinay <vinay@gmail.com>
2023-07-24 20:13:09 +0530

    added code new

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -1,16 +1,24 @@
 import com.i27academy.builds.Calculator
 
-
-pipeline {
-    agent any
-    stages {
-        stage("Echol") {
-            steps {
-               script {
-                 echo "printing sum of 2 numbers"
-                 println Calculator.add(2,8)
-               }
+def call(Map pipelineParams) {
+    Calculator calculator = new Calculator(this)
+    environment {
+        APP_NAME = "${pipelineParams.appName}"
+    }
+    pipeline {
+        agent any
+        stages {
+            stage("Echol") {
+                steps {
+                script {
+                    echo "printing sum of 2 numbers"
+                    println calculator.add(2,8)
+                    echo "microservice name is ${APP_NAME}"
+                }
+                }
             }
         }
     }
 }
+
+
